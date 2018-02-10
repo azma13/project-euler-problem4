e8fedac4bdb9482bf61271abdcd53768139734d4
Azma Sholiha <vy.phera@gmail.com>
2018-02-10 21:32:59 +0700

    1stcommit

---
+++ test/pali.js
@@ -0,0 +1,15 @@
+function pali() {
+  var max = 0;
+  for(var i = 999; i > 99; i--) {
+    for(var k = 999; k > 99; k--) {
+      var n = i * k;
+      var s = '' + n;
+      if (s === s.split("").reverse().join("") && n > max) {
+        max = n;
+      }
+    }
+  }
+  return max;
+}
+module.exports = pali;
+//console.log(palindromic());
---
+++ test/pali.test.js
@@ -0,0 +1,5 @@
+const pali = require('./pali');
+
+test('palindromic = 906609', () => {
+  expect(pali()).toBe(906609);
+});
